edf83aab84a8efc991b8da7e9c3e603e826c9060
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-09-28 00:03:55 -0300

    create registration component

---
+++ src/pages/Code.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text } from 'react-native';
+import Body from '../components/Body';
+import Container from '../components/Container';
+import Input from '../components/Input';
+import SimpleButton from '../components/SimpleButton';
+
+export default function Code() {
+
+  return (
+    <Container>
+     
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
